43bf1a016abbf4e785c9bc9fba757509feba35fb
SID20046 <sid20046@gmail.com>
2025-08-30 18:14:31 +0530

    added a centralized error and response handler

--- server/controller/user-contoller.js
+++ server/controller/user-contoller.js
@@ -1,71 +1,65 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const { ApiResponse } = require("../utils/ApiResponse");
+const { ApiError } = require("../utils/ApiError");
 
-const getAllUser = async(req,res,next) =>{
-    let users;
-
-    try{
-        users = await User.find();
+const getAllUser = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        if (!users || users.length === 0) {
+            return res.status(404).json(new ApiError(404, "Users not found"));
+        }
+        return res.status(200).json(new ApiResponse(200, { users }, "Users fetched successfully"));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new ApiError(500, "Server error while fetching users"));
     }
-    catch(err){
-        console.log(err);
+};
+
+const signUp = async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json(new ApiError(400, "User already exists"));
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            blogs: []
+        });
+
+        await user.save();
+        return res.status(201).json(new ApiResponse(201, { user }, "User registered successfully"));
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json(new ApiError(500, "Server error while signing up"));
     }
-    if(!users){
-        return res.status(404).json({ message : "users are not found"})
+};
+
+const logIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            return res.status(404).json(new ApiError(404, "User not found"));
+        }
+
+        const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json(new ApiError(400, "Incorrect Password"));
+        }
+
+        return res.status(200).json(new ApiResponse(200, { user: existingUser }, "Login successful"));
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json(new ApiError(500, "Server error while logging in"));
     }
+};
 
-    return res.status(200).json({users});
-}
-
-const signUp = async(req,res,next) =>{
-   const { name , email , password } = req.body;
-
-   let existingUser;
-
-   try{
-    existingUser = await User.findOne({email})
-   }catch(err){
-    console.log(err);
-   }
-
-   if(existingUser){
-       return res.status(400).json({message : "User is already exists!"})
-   }
-   const hashedPassword = bcrypt.hashSync(password);
-   const user = new User({
-       name,email,
-       password: hashedPassword,
-       blogs: []
-   });
-
-   try{
-       user.save();
-       return res.status(201).json({ user })
-   }
-   catch(e){console.log(e);}
-}
-
-const logIn = async(req,res,next) => {
-    const {email , password} = req.body;
-    
-    let existingUser;
-
-    try{
-     existingUser = await User.findOne({email})
-    }catch(err){
-     console.log(err);
-    }
-    if(!existingUser){
-        return res.status(404).json({message : "User is not found"})
-    }
-
-    const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password);
-
-    if(!isPasswordCorrect){
-        return res.status(400).json({message: "Incorrect Password!"});
-    }
-
-    return res.status(200).json({user: existingUser});
-}
-
-module.exports = { getAllUser, signUp , logIn};
+module.exports = { getAllUser, signUp, logIn };
